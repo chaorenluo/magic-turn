cd44148f1e5914e8fd086a4b3d3756b407db9365
ljm <ljm@addcn.com>
2022-11-22 18:41:30 +0800

    更改花括号的正则表达式用来可以匹配到花括号里面的换行

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,35 @@
+// import path from 'path'
+
+// export const options = {
+//   dataName: 'state',
+//   alias: {
+//     "~": "D:\/gitroot\/8591\/touch\/",
+//     "@": "D:\/gitroot\/8591\/touch\/",
+//   },
+//   piniaStore: {
+//     aliasPrefix: '~/store',
+//     pathVal:'D:\/gitroot\/8591\/touch\/store\/'
+//   },
+//   output:   path.join(__dirname, '..\/newVue\/'),
+//   entranceDir:'D:\/gitroot\/8591\/touch\/',
+//   compileDir:['components','pages']
+// }
+
 import path from 'path'
 
 export const options = {
-  dataName: 'state',
+  dataName: 'stateData',
   alias: {
-    "~": "D:\/gitroot\/8591\/touch\/",
-    "@": "D:\/gitroot\/8591\/touch\/",
+    "~": "/Users/ljm/gitroot/8591/touch/",
+    "@": "/Users/ljm/gitroot/8591/touch/",
   },
   piniaStore: {
     aliasPrefix: '~/store',
-    pathVal:'D:\/gitroot\/8591\/touch\/store\/'
+    pathVal:'/Users/ljm/gitroot/8591/touch/store/'
   },
-  output:   path.join(__dirname, '..\/newVue\/'),
-  entranceDir:'D:\/gitroot\/8591\/touch\/',
+  output:   path.join(__dirname, '../newVue/'),
+  entranceDir:'/Users/ljm/gitroot/8591/touch/',
   compileDir:['components','pages']
 }
+
 
